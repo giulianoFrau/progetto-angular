e593d7dc0eb59cef33a20fbf52436407a7f11013
giulianoFrau <shokteo@gmail.com>
2023-10-30 13:07:35 +0100

    feat: aggiunti hook

--- src/app/prova/prova.component.ts
+++ src/app/prova/prova.component.ts
@@ -1,13 +1,26 @@
-import { Component,OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewChecked, Component,OnDestroy,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-prova',
   templateUrl: './prova.component.html',
   styleUrls: ['./prova.component.css']
 })
-export class ProvaComponent implements OnInit {
-constructor(){}
+export class ProvaComponent implements OnInit,AfterViewChecked,AfterContentInit,AfterViewChecked,OnDestroy {
+constructor(){
+  console.log('costruttore')
+}
 ngOnInit(): void {
+    console.log('onInit')
+}
+ngAfterContentInit(): void {
+  console.log('afterContentInit')
+}
+ngAfterViewChecked(): void {
+  console.log('afterViewCheck')
+    
+}
+ngOnDestroy(): void {
+  console.log('destroy')
     
 }
 }
