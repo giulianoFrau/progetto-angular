86ea6130f6ea33cdf0d825c50349234c64e876dc
giulianoFrau <shokteo@gmail.com>
2024-10-14 17:46:33 +0200

    refact code

--- src/app/input-event-binding/input-event-binding.component.ts
+++ src/app/input-event-binding/input-event-binding.component.ts
@@ -3,25 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-input-event-binding',
   templateUrl: './input-event-binding.component.html',
-  styleUrls: ['./input-event-binding.component.css']
+  styleUrls: ['./input-event-binding.component.css'],
 })
 export class InputEventBindingComponent {
-  inputValue="test"
-  title="corso angular";
-  subtitle="test"
-  isVisible=true
-   
-/*   onInput(e:any){
+  inputValue = 'test';
+  title = 'corso angular';
+  subtitle = 'test';
+  isVisible = true;
+
+  /*   onInput(e:any){
     console.log(e.target.value) alternativa a giu più semplice
   } */
 
-  onInput(e:Event){
-    this.title=(<HTMLInputElement>e.target).value
-    console.log((<HTMLInputElement>e.target).value)
+  onInput(e: Event) {
+    this.title = (<HTMLInputElement>e.target).value;
+    console.log((<HTMLInputElement>e.target).value);
   }
 
-  onClick(e:any){
+  onClick(e: any) {
     // this.title='ho cliccato sul bottone'
-console.log(this.title + this.subtitle)
+    console.log(this.title + this.subtitle);
   }
 }
