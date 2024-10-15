373c3021e874c3d250acfc2801c618cee682f1f8
giulianoFrau <shokteo@gmail.com>
2024-10-15 11:06:15 +0200

    feat: passaggio props

--- src/app/prova2/prova2.component.ts
+++ src/app/prova2/prova2.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class Prova2Component implements OnInit {
   @Input() title: string = '';
+  @Input() test: boolean = false;
   constructor() {}
 
   ngOnInit(): void {}
