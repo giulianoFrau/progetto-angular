eec2fee6a4c005f997c258337bace6b91c44c3b0
giulianoFrau <shokteo@gmail.com>
2024-10-17 17:23:04 +0200

    refact code

--- src/app/input-event-binding/input-event-binding.component.ts
+++ src/app/input-event-binding/input-event-binding.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { ServizioProvaService } from '../servizi/servizio-prova.service';
 @Component({
   selector: 'app-input-event-binding',
   templateUrl: './input-event-binding.component.html',
@@ -9,7 +9,14 @@ export class InputEventBindingComponent {
   title = 'corso angular';
   subtitle = 'test';
 
+  constructor(private servizioProva: ServizioProvaService) {}
+
   onClick() {
     console.log(this.title + this.subtitle);
+  }
+
+  async fetchData() {
+    await this.servizioProva.fetchData();
+    console.log(this.servizioProva.data);
   }
 }
