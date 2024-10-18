224ada978175804f27df6fd1f4e380a71159b2da
giulianoFrau <shokteo@gmail.com>
2024-10-18 09:22:46 +0200

    refact code

--- src/app/componenti/contact/contact.component.ts
+++ src/app/componenti/contact/contact.component.ts
@@ -10,19 +10,18 @@ export class ContactComponent {
   persone: any = [];
   persona: any = {};
   isProfile: boolean = false;
+  urlId: any;
   constructor(
     private personeService: PersoneService,
     private route: ActivatedRoute
   ) {}
 
   ngOnInit(): void {
-    if (this.route.snapshot.paramMap.get('id')) {
-      this.isProfile = true;
-      this.persona = this.personeService.getPersonaByIndex(
-        parseInt(this.route.snapshot.paramMap.get('id')!)
-      );
+    this.urlId = this.route.snapshot.paramMap.get('id');
+    if (this.urlId) {
+      const index: number | any = parseInt(this.urlId) - 1;
+      this.persona = this.personeService.getPersonaByIndex(parseInt(index));
     } else {
-      this.isProfile = false;
       this.persone = this.personeService.getPersone();
     }
   }
