1e34f088eee63cb104d07eaac1cf1b7d9d7ebe48
sc-giulianoFrau <giuliano.frau1992@gmail.com>
2024-11-12 13:04:29 +0100

    refact

--- src/app/componenti/home/home.component.ts
+++ src/app/componenti/home/home.component.ts
@@ -25,3 +25,38 @@ export class HomeComponent {
     console.log(this.formData);
   }
 }
+
+/*
+**
+ * ALTERNATIVA A USARE SUBSCRIBE USANDO lastValueFrom
+ * 
+ * 
+ * 
+ * import { Component } from '@angular/core';
+import { FirebaseService } from 'src/app/servizi/firebase.service';
+import { lastValueFrom } from 'rxjs';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent {
+  formData: any = null;
+  isDataSend: boolean = false;
+
+  constructor(private firebase: FirebaseService) {}
+  async onSubmit(form: any) {
+    this.formData = form.form.value;
+    await lastValueFrom(
+      this.firebase.insertPersona(
+        'https://corso-angular-14ad1-default-rtdb.europe-west1.firebasedatabase.app/persone.json',
+        this.formData
+      )
+    );
+
+    console.log(this.formData);
+  }
+}
+
+ */
